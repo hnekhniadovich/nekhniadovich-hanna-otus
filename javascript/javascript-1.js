44e9b8c243399662119dfe37d767b265b521f040
Hanna Nekhniadovich <hanna@surfside.io>
2021-07-19 22:43:37 -0400

    homework 1 revisited 2

--- javascript/javascript-1.js
+++ javascript/javascript-1.js
@@ -3,48 +3,45 @@ const maxItemAssociation = (products) => {
         return null
     }
 
+    const sortedProducts = products.map(el => el.sort())
+    sortedProducts.sort()
+
     const associations = []
 
     let isVisited
 
-    for(let i = 0; i < products.length; i++) {
+    for(let i = 0; i < sortedProducts.length; i++) {
         isVisited = false
         if(!associations.length) {   
-            associations.push(new Set(products[i]))
+            associations.push(new Set(sortedProducts[i]))
         }
         else {
             for(let set of associations) {
-                for(let item of products[i]) {
+                for(let item of sortedProducts[i]) {
                     if(set.has(item)) {
                         isVisited = true
-                        products[i].forEach(item => set.add(item))
+                        sortedProducts[i].forEach(item => set.add(item))
                     }
                 }
             }
             if(!isVisited) {
-                associations.push(new Set(products[i]))
+                associations.push(new Set(sortedProducts[i]))
             }
         }
     }
 
-    const sortedAssociations = associations.map(el => [...el].sort())
-    sortedAssociations.sort()
-
     let max = 0
     let maxAssociation
 
-
-    for(let y = 0; y < sortedAssociations.length; y++) {
-        if(sortedAssociations[y].length > max) {
-            max = sortedAssociations[y].length
-            maxAssociation = sortedAssociations[y]
+    for(let y = 0; y < associations.length; y++) {
+        if(associations[y].size > max) {
+            max = associations[y].size
+            maxAssociation = associations[y]
         }
     }
 
-    return maxAssociation
+    return [...maxAssociation].sort()
 }
-
-
 
 
 
