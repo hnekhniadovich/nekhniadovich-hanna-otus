0ce8a2634e4cd3c6fd355a22de1d2c19addaa860
Hanna Nekhniadovich <hanna@surfside.io>
2021-08-06 12:57:44 -0400

    homework 3 revisited

--- javascript/javascript-3/index.js
+++ javascript/javascript-3/index.js
@@ -5,20 +5,29 @@
 //вызванный с этим селектором, не должен находить никаких элементов, 
 //кроме исходного.
 
-let element = document.querySelector('#container');
+let element = document.querySelector('.bar');
 
 const getPath = (el) => {
-    let path = [], parent;
-    while (parent = el.parentNode) {
-      let tag = el.tagName, siblings;
+    let path = [];
+    let parent;
+    
+    while (el.parentNode && el.tagName !== 'HTML') {
+      parent = el.parentNode;
+      let tag = el.tagName;
+      let siblings = parent.children;
       path.unshift(
-        el.id ? `#${el.id}` : (
-          siblings = parent.children,
-          [].filter.call(siblings, sibling => sibling.tagName === tag).length === 1 ? tag :
-          `${tag}:nth-child(${1+[].indexOf.call(siblings, el)})`
+        el.id ? `#${el.id}` : 
+        (
+          [...siblings].filter(sibling => sibling.tagName === tag).length === 1 
+            ? 
+          tag 
+            :
+          `${tag}:nth-child(${ 1 + [...siblings].indexOf(el) })`
         )
       );
       el = parent;
     };
     return `${path.join(' > ')}`.toLowerCase();
 };
+
+console.log('result', getPath(element))
--- javascript/javascript-3/index.test.js
+++ javascript/javascript-3/index.test.js
@@ -1,9 +1,14 @@
-it('getPath returns a correct selector', () => {
+it('getPath returns correct selector', () => {
     const container = document.querySelector('#container');
-    expect(getPath(container)).to.equal('html > body > #container');
+    expect(getPath(container)).to.equal('body > #container');
   });
   
-it('getPath returns a unique selector', () => {
+it('getPath returns unique selector', () => {
     const div = document.querySelector('.bar');
     expect(document.querySelectorAll(getPath(div)).length).to.equal(1);
+});
+
+it('getPath returns correct nth child', () => {
+  const div = document.querySelector('.bar');
+  expect(getPath(div)).to.equal('body > #container > div:nth-child(2) > div:nth-child(2)');
 });
