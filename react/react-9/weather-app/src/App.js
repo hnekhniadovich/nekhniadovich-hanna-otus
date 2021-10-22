31874c2c0b5ab43fed7da9d1aab669c62a3b9f84
Hanna Nekhniadovich <hanna@surfside.io>
2021-10-22 19:44:39 -0400

    home page added

---
+++ react/react-9/weather-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import HomePage from './components/HomePage';
+import 'antd/dist/antd.css';
+
+function App() {
+  return (
+    <div className="weather-app">
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
---
+++ react/react-9/weather-app/src/components/CityPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const CityPage = () => {
+    return (
+        <div>This is a city page</div>
+    )
+};
+
+export default CityPage;
